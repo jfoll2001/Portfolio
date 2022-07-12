6d75eb16f018224de7be04c83b43ab421307be6a
Samshalie <samshalie@icloud.com>
2022-07-12 09:12:27 -0400

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script>
 import Navbar from '@/components/Navbar.vue'
+import Header from '@/components/Header.vue'
+import Portfolio from '@/components/Portfolio.vue'
+import About from '@/components/About.vue'
+import Contact from '@/components/Contact.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Header,
+    Portfolio,
+    About,
+    Contact,
+    Footer
   }
 }
 </script>
@@ -13,6 +23,21 @@ export default {
   <body id="page-top">
     <!-- Navagation -->
     <Navbar />
+
+    <!-- Masthead-->
+    <Header />
+
+    <!-- Portfolio Section-->
+    <Portfolio />
+
+    <!-- About Section-->
+    <About />
+
+    <!-- Contact Section-->
+    <Contact />
+
+    <!-- Footer-->
+    <Footer />
   </body>
 
 </template>
